f2bd2842cfb0873bd3b7b9c7a306a317a8040d96
Lilit Hakobyan <Lilit.Hakobyan@helpsystems.com>
2022-07-21 15:07:16 +0400

    Add project files.

---
+++ WebSocketAspNetServer/Startup.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebSocketAspNetServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            // create options for web socket
+            var wsOptions = new WebSocketOptions
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(120)
+            };
+            app.UseWebSockets(wsOptions);
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/send")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        await Send(webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    }
+                }
+            });
+        }
+
+        private static async Task Send(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            while (!result.CloseStatus.HasValue)
+            {
+                var message = Encoding.UTF8.GetString(new ArraySegment<byte>(buffer, 0, result.Count));
+                Console.WriteLine($"Received from client: {message}");
+
+                // send back to client
+                await webSocket.SendAsync(
+                    new ArraySegment<byte>(
+                        Encoding.UTF8.GetBytes($"Server received the message on {DateTime.UtcNow:f}")),
+                    result.MessageType,
+                    result.EndOfMessage,
+                    CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                //  Console.WriteLine(result);
+            }
+
+            if (result.MessageType == WebSocketMessageType.Close)
+            {
+                Console.WriteLine($"Connection closed: Status: {result.CloseStatus.Value} , Description: {result.CloseStatusDescription}");
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+}
---
+++ WebSocketClient/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketClient
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Press enter to connect .................");
+            Console.ReadLine();
+
+            using var client = new ClientWebSocket();
+
+            var serviceUri = new Uri("ws://localhost:5000/send");
+
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(120));
+
+            try
+            {
+                await client.ConnectAsync(serviceUri, cancellationTokenSource.Token);
+                while (client.State == WebSocketState.Open)
+                {
+                    Console.WriteLine("Enter message to send");
+                    var message = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(message))
+                    {
+                        continue;
+                    }
+
+                    if (message.TrimEnd().ToLower() == "close")
+                    {
+                      await  client.CloseAsync(WebSocketCloseStatus.NormalClosure, "Connection closed by client",
+                            cancellationTokenSource.Token);
+                      break;
+                    }
+
+                    var byteToSend = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+                    await client.SendAsync(byteToSend, WebSocketMessageType.Text, true, cancellationTokenSource.Token);
+
+
+                    var responseBuffer = new byte[1024];
+                    while (true)
+                    {
+                        var byteReceived = new ArraySegment<byte>(responseBuffer, 0, 1024);
+                        var wsReceivedResult = await client.ReceiveAsync(byteReceived, cancellationTokenSource.Token);
+
+                        if (wsReceivedResult.MessageType == WebSocketMessageType.Close)
+                        {
+                            var status = wsReceivedResult.CloseStatus.HasValue
+                                ? wsReceivedResult.CloseStatus.Value.ToString()
+                                : "unknown";
+
+                            Console.WriteLine($"Connection closed: Status: {status} , Description: {wsReceivedResult.CloseStatusDescription}");
+                        }
+
+                        var responseMessage = Encoding.UTF8.GetString(responseBuffer, 0, wsReceivedResult.Count);
+
+                        Console.WriteLine(responseMessage);
+
+                        if (wsReceivedResult.EndOfMessage)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                if (client.State == WebSocketState.CloseReceived)
+                {
+                    Console.WriteLine("Closed received from the server");
+                }
+
+            }
+            catch (WebSocketException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
